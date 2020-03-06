1e9af99342d1a0357654cf66d0b57a8a357735ea
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-03-06 04:24:46 +0300

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ def get_text_messages(message):
  if message.text ==u"/помощь":
    bot.send_message(message.chat.id, u"Доступные команды  /новости /информация")
  if message.text ==u"/новости":
-   bot.send_message(message.chat.id, u"Нет никаких новостей!")
+   bot.send_message(message.chat.id, u"IP SAMP RP сервера сменился 45.142.214.154:7777!")
  if message.text ==u"/информация":
-    bot.send_message(message.chat.id, u"Открыт SAMP RP Сервер! IP: 109.234.38.237:7777 Полезные ссылки: Наш сайт - laciamemeframe.space (Сайт создателя проекта, так как сайт самого сервера находится в разработке). Форум - В разработке, Группа Вконтакте - https://vk.com/pixsetup, Свободная группа Вконтакте - В разработке. IP: 109.234.38.237:7777")
+    bot.send_message(message.chat.id, u"Открыт SAMP RP Сервер! IP: 45.142.214.154:7777 Полезные ссылки: Наш сайт - laciamemeframe.space (Сайт создателя проекта, так как сайт самого сервера находится в разработке). Форум - В разработке, Группа Вконтакте - https://vk.com/pixsetup, Свободная группа Вконтакте - В разработке. IP: 45.142.214.154:7777")
  #elif message.text =="/help":
    #bot.send_message(message.from_user.id, "Доступные команды  /news и /info")
  #else:
