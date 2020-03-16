c9e042ad4a965b2832e0274c1353c0043ee30f64
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-03-16 14:19:12 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,11 +6,17 @@ bot = telebot.TeleBot('token');
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
  if message.text ==u"/помощь":
-   bot.send_message(message.chat.id, u"Доступные команды  /новости /информация")
+   bot.send_message(message.chat.id, u"Доступные команды  /новости /информация /помощь")
+ if message.text ==u"/help":
+   bot.send_message(message.chat.id, u"Доступные команды  /новости /информация /помощь") 
  if message.text ==u"/новости":
    bot.send_message(message.chat.id, u"IP SAMP RP сервера сменился 34.91.233.9:7777!")
+ if message.text ==u"/news":
+   bot.send_message(message.chat.id, u"IP SAMP RP сервера сменился 34.91.233.9:7777!")  
  if message.text ==u"/информация":
     bot.send_message(message.chat.id, u"Открыт SAMP RP Сервер! IP: 34.91.233.9:7777 Полезные ссылки: Наш сайт - laciamemeframe.space (Сайт создателя проекта, так как сайт самого сервера находится в разработке). Форум - В разработке, Группа Вконтакте - https://vk.com/pixsetup, Свободная группа Вконтакте - В разработке. IP: 34.91.233.9:7777")
+ if message.text ==u"/info":
+    bot.send_message(message.chat.id, u"Открыт SAMP RP Сервер! IP: 34.91.233.9:7777 Полезные ссылки: Наш сайт - laciamemeframe.space (Сайт создателя проекта, так как сайт самого сервера находится в разработке). Форум - В разработке, Группа Вконтакте - https://vk.com/pixsetup, Свободная группа Вконтакте - В разработке. IP: 34.91.233.9:7777")     
  #elif message.text =="/help":
    #bot.send_message(message.from_user.id, "Доступные команды  /news и /info")
  #else:
