c606dc12673790cb9ff8c1adc142734a38016943
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-02 23:50:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,16 +7,26 @@ bot = telebot.TeleBot('token');
 def get_text_messages(message):
  if message.text ==u"/помощь":
    bot.send_message(message.chat.id, u"Доступные команды: /новости /информация /помощь")
- if message.text ==u"/help":
+ if message.text ==u"/help@pixelsetup_bot":
    bot.send_message(message.chat.id, u"Доступные команды: /новости /информация /помощь") 
+ if message.text ==u"/help":
+   bot.send_message(message.chat.id, u"Доступные команды: /новости /информация /помощь")   
  if message.text ==u"/новости":
    bot.send_message(message.chat.id, u"IP SAMP RP сервера сменился 34.91.233.9:7777!")
+ if message.text ==u"/news@pixelsetup_bot":
+   bot.send_message(message.chat.id, u"IP SAMP RP сервера сменился 34.91.233.9:7777!")
  if message.text ==u"/news":
-   bot.send_message(message.chat.id, u"IP SAMP RP сервера сменился 34.91.233.9:7777!")  
+   bot.send_message(message.chat.id, u"IP SAMP RP сервера сменился 34.91.233.9:7777!")   
  if message.text ==u"/информация":
     bot.send_message(message.chat.id, u"Открыт SAMP RP Сервер! IP: 34.91.233.9:7777 Полезные ссылки: Наш сайт - laciamemeframe.space (Сайт создателя проекта, так как сайт самого сервера находится в разработке). Форум - В разработке, Группа Вконтакте - https://vk.com/pixsetup, Свободная группа Вконтакте - В разработке. IP: 34.91.233.9:7777")
+ if message.text ==u"/info@pixelsetup_bot":
+    bot.send_message(message.chat.id, u"Открыт SAMP RP Сервер! IP: 34.91.233.9:7777 Полезные ссылки: Наш сайт - laciamemeframe.space (Сайт создателя проекта, так как сайт самого сервера находится в разработке). Форум - В разработке, Группа Вконтакте - https://vk.com/pixsetup, Свободная группа Вконтакте - В разработке. IP: 34.91.233.9:7777")
  if message.text ==u"/info":
-    bot.send_message(message.chat.id, u"Открыт SAMP RP Сервер! IP: 34.91.233.9:7777 Полезные ссылки: Наш сайт - laciamemeframe.space (Сайт создателя проекта, так как сайт самого сервера находится в разработке). Форум - В разработке, Группа Вконтакте - https://vk.com/pixsetup, Свободная группа Вконтакте - В разработке. IP: 34.91.233.9:7777")     
+    bot.send_message(message.chat.id, u"Открыт SAMP RP Сервер! IP: 34.91.233.9:7777 Полезные ссылки: Наш сайт - laciamemeframe.space (Сайт создателя проекта, так как сайт самого сервера находится в разработке). Форум - В разработке, Группа Вконтакте - https://vk.com/pixsetup, Свободная группа Вконтакте - В разработке. IP: 34.91.233.9:7777")       
+ if message.text ==u"/pidor_stat@pixelsetup_bot": 
+    bot.send_message(message.chat.id, u"привет пидор")
+ if message.text ==u"/pidor_stat": 
+    bot.send_message(message.chat.id, u"привет пидор")
  #elif message.text =="/help":
    #bot.send_message(message.from_user.id, "Доступные команды  /news и /info")
  #else:
