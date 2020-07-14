081ef59dc5396950a26c58a202a963cc2500fcb6
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-07-14 03:56:30 +0300

    update

--- test.py
+++ test.py
@@ -11,7 +11,8 @@ bot = telebot.TeleBot('токен');
 def handle_grammar2(m):
     keyboard = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
     pussy = types.KeyboardButton(u'🐈pussy')
-    keyboard.row(pussy)
+    dogs = types.KeyboardButton(u'🦮dogs')
+    keyboard.row(pussy, dogs)
     tg_analytic.statistics (m.chat.id, m.text)
     bot.send_message(m.from_user.id, u'Добро пожаловать', reply_markup=keyboard)
 
@@ -47,8 +48,51 @@ def query_photo(inline_query):
     except Exception as e:
         print(e)     
 
+@bot.inline_handler(lambda query: query.query == u'dogs')
+def query_photo(inline_query):
+    try:
+        r = types.InlineQueryResultPhoto('1',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline.jpg',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline.jpg')      
+        r2 = types.InlineQueryResultPhoto('2',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline1.jpg',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline1.jpg')     
+        r3 = types.InlineQueryResultPhoto('3',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline2.jpg',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline2.jpg')     
+        r4 = types.InlineQueryResultPhoto('4',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline3.jpg',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline3.jpg')    
+        r5 = types.InlineQueryResultPhoto('5',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline4.jpg',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline4.jpg')
+        r6 = types.InlineQueryResultPhoto('6',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline5.jpg',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline5.jpg')
+        r7 = types.InlineQueryResultPhoto('7',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline6.jpg',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline6.jpg') 
+        r8 = types.InlineQueryResultPhoto('8',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline7.jpg',
+                                          'https://raw.githubusercontent.com/LaciaMemeFrame/ChatBot-Telegram/master/inline7.jpg')                                   
+
+        bot.answer_inline_query(inline_query.id, [r, r2, r3, r4, r5, r6, r7, r8], cache_time=1)
+    except Exception as e:
+        print(e)     
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+ if message.text == u'/dogs' or message.text == u'/dogs@pixelsetup_bot' or message.text == u'🦮dogs':  
+    url = 'https://api.thedogapi.com/v1/images/search'
+    r = requests.get(url, allow_redirects=True)
+    r.headers['x-api-key'] = 'токен'
+    json = r.json()
+    for j in json:
+      dogsurl = j['url']
+      rs = requests.get(dogsurl, allow_redirects=True)
+      open('dogs.jpg', 'wb').write(rs.content)
+      tg_analytic.statistics (message.chat.id, message.text)
+      bot.send_photo(message. chat.id, open("dogs.jpg","rb"))
  if message.text == u'/pussy' or message.text == u'/pussy@pixelsetup_bot' or message.text == u'🐈pussy':  
     url = 'https://api.thecatapi.com/v1/images/search'
     r = requests.get(url, allow_redirects=True)
